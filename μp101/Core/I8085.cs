0abc3e7cc94f4bd4a49d637603323a5d12d0509f
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-14 20:45:33 +0530

    core structs

---
+++ μp101/Core/I8085.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace μp101.Core
+{
+    public static class I8085
+    {
+        public static Memory[] Memory = new Memory[65536];
+    }
+    public class Register
+    {
+        public string Name { get; }
+        public Memory Value { get; set; }
+        public Register Pair { get; } = null;
+        public Register(string n,Memory v,Register pair=null)
+        {
+            Name = n;
+            Value = v;
+            Pair = pair;
+        }
+    }
+    public struct Memory
+    {
+        public byte Data { get; set; }
+        public string Hex => Data.ToString("X2");
+    }
+}
