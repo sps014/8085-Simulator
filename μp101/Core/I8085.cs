4a678d3d249663fe8c7f593db2f6383ce5ca8fcf
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-14 21:22:35 +0530

    table todo

--- μp101/Core/I8085.cs
+++ μp101/Core/I8085.cs
@@ -10,13 +10,13 @@ namespace μp101.Core
     public static class I8085
     {
         public static Memory[] Memory { get; } = new Memory[65536];
-        public static Register A {get;} = new Register("A");
-        public static Register B {get;}= new Register("B");
-        public static Register C {get;}= new Register("C",B);
-        public static Register D {get;}= new Register("D");
-        public static Register E {get;}= new Register("E",D);
-        public static Register H {get;}= new Register("H");
-        public static Register L {get;}= new Register("L",H);
+        public static Register A { get; } = new Register("A");
+        public static Register B { get; } = new Register("B");
+        public static Register C { get; } = new Register("C", B);
+        public static Register D { get; } = new Register("D");
+        public static Register E { get; } = new Register("E", D);
+        public static Register H { get; } = new Register("H");
+        public static Register L { get; } = new Register("L", H);
 
     }
     public class Register
@@ -24,7 +24,7 @@ namespace μp101.Core
         public string Name { get; }
         public Memory Value { get; set; }
         public Register Pair { get; } = null;
-        public Register(string n,Register pair=null, Memory v = new Memory())
+        public Register(string n, Register pair = null, Memory v = new Memory())
         {
             Name = n;
             Value = v;
@@ -35,5 +35,15 @@ namespace μp101.Core
     {
         public byte Data { get; set; }
         public string Hex => Data.ToString("X2");
+    }
+    public struct Flag
+    {
+        public string Name { get; }
+        public bool Value { get; set; }
+        public Flag(string n, bool v = false)
+        {
+            Name = n;
+            Value = v;
+        }
     }
 }
