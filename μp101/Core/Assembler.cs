cee993538b1e5540c1475c2877e3636882d32bff
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-19 17:29:48 +0530

    Create Assembler.cs

---
+++ μp101/Core/Assembler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace μp101.Core
+{
+    public static class Assembler
+    {
+        private static Dictionary<string, object> MnmonicsExecuter = new Dictionary<string, object>();
+        public static LineAssembleResult ExecuteLine(string line)
+        {
+
+        }
+        private static LineAssembleResult FetchMainWord(string line)
+        {
+
+        }
+
+    }
+}
