f16b6f18df1f7a349e6930f708fc912d1ebf813b
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-19 19:57:05 +0530

    Create InstructionSet.cs

---
+++ μp101/Core/InstructionSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace μp101.Core
+{
+    public static class InstructionSet
+    {
+        public static void MOV(string line,LineAssembleResult result)
+        {
+
+        }
+    }
+}
