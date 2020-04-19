0287e5bc47d4f1018a2d82902e2f625ca36ffc2a
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-19 20:18:59 +0530

    utility

---
+++ μp101/Core/AssemblyUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace μp101.Core
+{
+    public static class AssemblyUtility
+    {
+        public static Register IsRegister(string word)
+        {
+            switch(word)
+            {
+                case "A":
+                    return I8085.A;
+                case "B":
+                    return I8085.B;
+                case "C":
+                    return I8085.C;
+                case "D":
+                    return I8085.D;
+                case "E":
+                    return I8085.E;
+                case "H":
+                    return I8085.H;
+                case "L":
+                    return I8085.L;
+                default:
+                    return null;
+            }
+        }
+    }
+}
