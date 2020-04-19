3a412a982002b8479266813fbb4fd77da45aeb76
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-19 19:51:13 +0530

    assembler todo

--- μp101/Core/Assembler.cs
+++ μp101/Core/Assembler.cs
@@ -1,21 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace μp101.Core
 {
     public static class Assembler
     {
-        private static Dictionary<string, object> MnmonicsExecuter = new Dictionary<string, object>();
+        private static Dictionary<string, Processable> MnmonicsExecuter = new Dictionary<string, Processable>();
         public static LineAssembleResult ExecuteLine(string line)
         {
+            if(string.IsNullOrWhiteSpace(line))
+            {
+                return new LineAssembleResult();
+            }
 
+            LineAssembleResult result = new LineAssembleResult();
+
+            string word = FetchMainWord(line,result);
+
+            return result;
         }
-        private static LineAssembleResult FetchMainWord(string line)
+        private static string FetchMainWord(string line,LineAssembleResult result)
         {
+            
+            var match=Regex.Match(line, @"\s*(\w+)\s*");
+            if(match.Success)
+            {
+                if(match.Groups[0].Value.Length>=1)
+                {
+                    return match.Groups[0].Value;
+                }
+            }
 
+            result.Result = AssembleOutcome.Failed;
+            result.ErrorMessage = "Invalid instruction found , can not  be processed.";
+
+            return null;
         }
+
+        public delegate void Processable(string line, LineAssembleResult result);
 
     }
 }
--- μp101/Core/LineAssemblyResult.cs
+++ μp101/Core/LineAssemblyResult.cs
@@ -11,8 +11,8 @@ namespace μp101.Core
         public List<Memory> MemoriesChanged { get; set; } = new List<Memory>();
         public List<Register> RegistersChanged { get; set; } = new List<Register>();
         public List<Flag> FlagsChanged { get; set; } = new List<Flag>();
-        public AssembleOutcome Result { get; set; }
-        public string ErrorMessage { get; set; }
+        public AssembleOutcome Result { get; set; } = AssembleOutcome.Success;
+        public string ErrorMessage { get; set; } = "";
     }
     
     public enum AssembleOutcome
