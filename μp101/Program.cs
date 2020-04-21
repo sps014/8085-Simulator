dcc41fe8f60397739d4b819303c26bd328f9967c
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-21 17:31:17 +0530

    Update Program.cs

--- μp101/Program.cs
+++ μp101/Program.cs
@@ -15,7 +15,9 @@ namespace μp101
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(new HttpClient 
+            { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
             await builder.Build().RunAsync();
         }
     }
