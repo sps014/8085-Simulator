7de8b8a8043280d54746fff6c263873d30e9bff8
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-21 19:36:17 +0530

    update layout

--- μp101/wwwroot/js/Asm86.js
+++ μp101/wwwroot/js/Asm86.js
@@ -1,6 +1,6 @@
 ﻿CodeMirror.defineMode("asm86", function () {
 	var keywords1 = /^(mov|mvi|add|adi)\b/i;
-	var keywords2 = /^(call|int|iret|ret|ja|jae|jb|jbe|jc|je|jg|jge|jl|jle|jmp|jna|jnae|jnb|jnbe|jnc|jne|jng|jnge|jnl|jnle|jno|jns|jnz|jo|js|jz)\b/i;
+	var keywords2 = /^(hlt|HLT)\b/i;
 	var keywords3 = /^(e?[abcd]x|[abcd][lh]|e?(si|di|bp|sp)|eip)\b/i;
 	var keywords4 = /^(d?word|byte|ptr)\b/i;
 	var numbers = /^(0x[0-9a-f]+|0b[01]+|[0-9]+|[0-9][0-9a-f]+h|[0-1]+b)\b/i;
