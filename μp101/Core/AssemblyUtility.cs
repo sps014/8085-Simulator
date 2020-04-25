0945195d8016a9bec55af427e56461328cf5a33e
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-04-25 18:56:47 +0530

    Update AssemblyUtility.cs

--- μp101/Core/AssemblyUtility.cs
+++ μp101/Core/AssemblyUtility.cs
@@ -31,9 +31,52 @@ namespace μp101.Core
                     return null;
             }
         }
-        public static void AddAdjustFlags(string value1,string valu2)
+        public static void AddAdjustFlags(byte value1,byte value2)
         {
+            string val1 = Convert.ToString(value1, 2).PadLeft(8, '0'); 
+            string val2 = Convert.ToString(value2, 2).PadLeft(8, '0');
 
+            string sum = "";
+            byte c = 0;
+            for(int i=7;i>=0;i--)
+            {
+                byte s = (byt)val1[i];
+            }
+
+        }
+        public static (byte,byte) AddBit(byte a,byte b,byte c=0)
+        {
+            byte s=0;
+            if (c == 0)
+            {
+                if (a == 0 && b == 0) 
+                {
+                    return (0, 0);
+                }
+                else if((a==0 && b==1 )|| (a == 1 && b==0))
+                {
+                    return (1, 0);
+                }
+                else
+                {
+                    return (0, 1);
+                }
+            }
+            else
+            {
+                if (a == 0 && b == 0)
+                {
+                    return (1, 0);
+                }
+                else if ((a == 0 && b == 1) || (a == 1 && b == 0))
+                {
+                    return (0, 1);
+                }
+                else
+                {
+                    return (1, 1);
+                }
+            }
         }
     }
 }
